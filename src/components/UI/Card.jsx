d6b56904bc01a0ad4bd4d73801e031fdd0762c62
Bektur12 <kanybekovbektur22@gmail.com>
2023-04-15 03:03:56 +0600

    add slider

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,79 @@
+import styled from "@emotion/styled";
+import { MySlider } from "../slider/MySlider";
+import { Button } from "./Button";
+
+export const Card = ({ image, price, bedrooms, bath, sq }) => {
+  return (
+    <Container>
+      <MySlider />
+      <HouseInfo>
+        <TopPart>
+          <h1 className="price">100.000 $</h1>
+          <Button>View Details</Button>
+        </TopPart>
+        <InfoContainer>
+          <Info>1 Bedrooms</Info>
+          <Info
+            border={{
+              left: "1px solid rgba(255, 251, 251, 0.35)",
+              rigth: "1px solid rgba(255, 251, 251, 0.35)",
+              width: "200px",
+            }}
+          >
+            1 Bath
+          </Info>
+          <Info> 535 sq ft</Info>
+        </InfoContainer>
+      </HouseInfo>
+    </Container>
+  );
+};
+
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+const HouseInfo = styled("div")`
+  background: #2c2b2b;
+  border-radius: 0px 0px 4px 4px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  padding: 16px 16px;
+`;
+
+const Info = styled("h2")`
+  border-left: ${(p) => p.border?.left};
+  border-right: ${(p) => p.border?.rigth};
+  width: ${(p) => p.border?.width};
+`;
+
+const InfoContainer = styled("div")`
+  display: flex;
+  justify-content: space-around;
+  border: 1px solid rgba(255, 251, 251, 0.35);
+  border-radius: 8px;
+  height: 42px;
+  h2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: rgba(255, 251, 251, 0.65);
+    font-family: "Montserrat";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+  }
+`;
+
+const TopPart = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  .price {
+    color: #fffbfb;
+  }
+`;
---
+++ src/components/slider/MySlider.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import prev from "../../assets/icon/arrowLeft.svg";
+import next from "../../assets/icon/nextArrow.svg";
+import styled from "@emotion/styled";
+import house from "../../assets/image/house.png";
+import { Box } from "@mui/material";
+
+const PrevArrow = ({ onClick }) => (
+  <PrevIcon onClick={onClick} src={prev} alt="prev-arrow" />
+);
+const NextArrow = ({ onClick }) => (
+  <NextIcon onClick={onClick} src={next} alt="next-arrow" />
+);
+
+export const MySlider = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
+  return (
+    <SliderContainer>
+      <Slider {...settings}>
+        <div>
+          <Image src={house} alt="" />
+        </div>
+        <div>
+          <Image src={house} alt="" />
+        </div>
+        <div>
+          <Image src={house} alt="" />
+        </div>
+      </Slider>
+    </SliderContainer>
+  );
+};
+
+const SliderContainer = styled(Box)``;
+
+const PrevIcon = styled("img")`
+  position: absolute;
+  left: 700px;
+  top: 250px;
+  cursor: pointer;
+  z-index: 2;
+`;
+const NextIcon = styled("img")`
+  position: absolute;
+  top: 250px;
+  left: 30px;
+  cursor: pointer;
+  z-index: 2;
+`;
+
+const Image = styled("img")`
+  width: inherit;
+`;
